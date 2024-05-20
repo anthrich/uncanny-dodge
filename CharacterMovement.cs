4e8cf6fbee574f206c7995d820c3d4e1edb00626
Anth Richardson <anthrblahblah@gmail.com>
2024-05-20 20:37:01 +0100

    Added character selection

--- CharacterMovement.cs
+++ CharacterMovement.cs
@@ -7,15 +7,23 @@ public partial class CharacterMovement : CharacterBody2D
     private Vector2 _targetPosition;
     private bool _isSelected;
     [Export] public float Speed = 50f;
+    [Export] public Sprite2D SelectedSprite2D;
+
+    public override void _Ready()
+    {
+        _targetPosition = Position;
+    }
 
     public void Select()
     {
         _isSelected = true;
+        SelectedSprite2D.Visible = true;
     }
 
     public void Deselect()
     {
         _isSelected = false;
+        SelectedSprite2D.Visible = false;
     }
     
     public override void _Input(InputEvent @event)
